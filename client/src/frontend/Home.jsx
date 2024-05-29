64a381b32919905d48652da756301f9a443515c8
codewools <201400044@gift.edu.pk>
2024-05-29 13:57:19 +0500

    By Daniyal: Update Home.jsx
    Just make hassan and my Home.jsx code file same.

--- client/src/frontend/Home.jsx
+++ client/src/frontend/Home.jsx
@@ -1,8 +1,9 @@
 import Header from "./component/Header";
 import Sidebar from "./component/SideBar";
 import LeftSidebar from "./LeftSideBar";
+import CollaboratorList from "./component/Collabler/CollaboratorList";
 import { PostCall } from "./component/NewsFeed";
-<<<<<<< Updated upstream
+
 import "../App.css";
 
 export function MainPage() {
@@ -23,24 +24,4 @@ export function MainPage() {
       </div>
     </div>
   );
-=======
-import CollaboratorList from "./component/Collabler/CollaboratorList";
-import "../App.css"; 
-
-export function MainPage() {
-    return (
-        <div className="flex flex-wrap gap-1 h-screen" style={{width: '100vw'}}>
-            <div className="flex-none h-full overflow-y-auto custom-scrollbar ">
-                <Sidebar />
-            </div>
-            <div className="flex-auto bg-gray-100 h-full overflow-y-auto custom-scrollbar"style={{width: '60vw'}}>
-                <Header />
-                <PostCall />
-            </div>
-            <div className="flex w-1/5 justify-center h-full overflow-y-auto custom-scrollbar">
-                <LeftSidebar />
-            </div>
-        </div>
-    );
->>>>>>> Stashed changes
 }
