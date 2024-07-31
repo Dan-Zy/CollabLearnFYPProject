2919d04d02f48092310da2a67178087d2f15d05c
Daniyal Zafar <201400044@gift.edu.pk>
2024-08-01 02:18:41 +0500

    Update Add Member to Community
    In this commit I added a functionality such that when user wants to join to community than it also added on the group chat (discussion forum) of that community.

--- server/controllers/communityControllers/addMemberToCommunity.js
+++ server/controllers/communityControllers/addMemberToCommunity.js
@@ -1,4 +1,5 @@
 import Community from "../../models/communityModel.js";
+import Chat from "../../models/chatModel.js";
 
 const addMemberToCommunity = async (req, res) => {
   try {
@@ -7,18 +8,38 @@ const addMemberToCommunity = async (req, res) => {
     console.log('User ID:', userId);
 
     const community = await Community.findById(communityId);
+    const discussionForum = await Chat.findOne({chatName: communityId});
+
+    console.log("COMMUNITY: ", community);
+    console.log("DISCUSSION FORUM: ", discussionForum);
+
     if (!community) {
       return res.status(404).json({ success: false, message: "Community not found" });
     }
 
-    if (community.members.includes(userId)) {
+    if(!discussionForum){
+      return res.status(400).json({
+        success: false,
+        message: `Error while fetching Discussion Forum of Community ${community.communityName}`
+      });
+    }
+
+    if (community.members.includes(userId) && discussionForum.users.includes(userId)) {
       return res.status(400).json({ success: false, message: "User is already a member of the community" });
     }
 
     community.members.push(userId);
+    discussionForum.users.push(userId)
     await community.save();
+    await discussionForum.save();
 
-    res.status(200).json({ success: true, message: "User added to the community", community });
+    res.status(201).json({ 
+      success: true, 
+      message: "User added to the community", 
+      community: community,
+      discussionForum: discussionForum
+    });
+    
   } catch (error) {
     console.error("Error while adding member to community:", error);
     res.status(500).json({ success: false, message: "Internal server error" });
