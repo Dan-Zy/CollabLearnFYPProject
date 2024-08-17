1e70e736725933f6e854dd062de45ca428a34080
Daniyal Zafar <201400044@gift.edu.pk>
2024-08-17 14:55:39 +0500

    Get Community Events Functionality
    In this commit I added a functionality to get community's events by giving communityId

--- server/routes/communityLiveSpaceRoutes.js
+++ server/routes/communityLiveSpaceRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authorization.js";
 import createEventCommunity from "../controllers/communityLiveSpaceControllers/createEventCommunity.js";
+import { getCommunityLiveSpaces } from "../controllers/communityLiveSpaceControllers/getCommunityEvents.js";
 
 
 const router = express.Router();
 
 router.post("/createEventCommunity/:communityId", verifyToken, createEventCommunity);
 
+router.get("/getCommunityEvents/:communityId", verifyToken, getCommunityLiveSpaces);
 
 export default router;
